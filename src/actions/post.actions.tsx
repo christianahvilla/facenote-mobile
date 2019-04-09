83b9a6c7e16445c79844e1326400351dd9fe06a7
Christian Alejandro Herrejón Villa <caherrejon@scio.local>
2019-04-09 14:45:07 -0500

    add actions

---
+++ src/actions/post.actions.tsx
@@ -0,0 +1,49 @@
+import * as types from "../types/post.types"
+import { postConstant } from "../constants/post.constant";
+import PostModel from "../models/post.model";
+import { Dispatch } from "redux";
+import { API } from "../api/post.api";
+
+
+
+/**
+ * Consts to get posts from API implementing API request convention
+ * @function fetchDataBegin
+ * @function fetchDataSuccess
+ * @function fetchDataError
+ */
+
+const fetchDataBegin = (): types.IActionFetchBegin => {
+    return {
+        type: postConstant.FETCH_POST_BEGIN
+    }
+}
+
+const fetchDataError = (e: Error): types.IActionFetchError => {
+    return {
+        type: postConstant.FETCH_POST_ERROR,
+        errorMessage: e.message
+    }
+}
+
+const fetchDataSuccess = (posts: Array<PostModel>): types.IActionFetchSuccess => {
+    return {
+        type: postConstant.FETCH_POST_SUCCESS,
+        posts
+    }
+}
+
+/**
+ * Function to get posts from API implementing API request convention
+ */
+
+export const getPosts = () => {
+    return (dispatch: Dispatch) => {
+        dispatch(fetchDataBegin())
+        API.getPosts().then(response => {
+            dispatch(fetchDataSuccess(response.data))
+        }).catch((error: Error) => {
+            dispatch(fetchDataError(error))
+        })
+    }
+}
