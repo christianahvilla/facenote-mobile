93fd7600f076952963f9a00a3d545c2fc7f07162
Christian Alejandro Herrejón Villa <caherrejon@scio.local>
2019-04-09 14:26:23 -0500

    make export their

--- src/types/post.types.tsx
+++ src/types/post.types.tsx
@@ -2,16 +2,16 @@ import { Action } from "redux";
 import { postConstant } from "../constants/post.constant";
 import PostModel from "../models/post.model";
 
-interface IActionFetchBegin extends Action {
+export interface IActionFetchBegin extends Action {
     type: postConstant.FETCH_POST_BEGIN
 }
 
-interface IActionFetchError extends Action {
+export interface IActionFetchError extends Action {
     type: postConstant.FETCH_POST_ERROR,
     errorMessage: string
 }
 
-interface IActionFetchSuccess extends Action {
+export interface IActionFetchSuccess extends Action {
     type: postConstant.FETCH_POST_SUCCESS,
     posts: Array<PostModel>
 }
